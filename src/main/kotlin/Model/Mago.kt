0b2df209ec6ae7a5ce6ea79388704e1d3963261a
Inara <inaraoenning@gmail.com>
2026-05-11 16:04:43 -0300

    Add classe Mago

---
+++ src/main/kotlin/Model/Mago.kt
@@ -0,0 +1,12 @@
+package Model
+import Personagem
+
+class Mago(nome: String, forca: Int, velocidade: Int, vida:Int, var magia : Int)
+    :Personagem(nome= nome, forca = forca, velocidade = velocidade, vida = vida) {
+
+    override fun usarPoder() {
+        this.vida += magia;
+        this.forca += magia;
+        println("$nome usou Magia aumentando Força e Vida em $magia!")
+    }
+}
