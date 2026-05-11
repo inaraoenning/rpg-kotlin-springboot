012f55de90792e125d9379dff3bac2e85ef00bf9
Inara <inaraoenning@gmail.com>
2026-05-11 16:00:30 -0300

    Add classe ladino

--- src/main/kotlin/Model/Ladino.kt
+++ src/main/kotlin/Model/Ladino.kt
@@ -1,8 +1,22 @@
 package Model
 
+import Personagem
 import jakarta.persistence.*
 
 @Entity
 @DiscriminatorValue("LADINO")
-class Ladino() {
+class Ladino(nome: String, forca: Int, velocidade: Int, vida: Int, var sagacidade: Int) :
+    Personagem(nome = nome, forca = forca, velocidade = velocidade, vida = vida) {
+
+    override fun usarPoder() {
+        super.usarPoder()
+        println("O Ladino está em modo furtivo!")
+    }
+
+    fun usarPoderAlvo(adversario: Personagem) {
+        println("$nome ignora a força bruta e usa sagacidade!")
+
+        // chama classe mae e passa sagacidade no valor do dano
+        super.atacar(adversario, danoEspecial = sagacidade)
+    }
 }
